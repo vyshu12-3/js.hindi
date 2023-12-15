a9748641f8c823c36512c05f6057b64efe610f74
vyshu12-3 <152533992+vyshu12-3@users.noreply.github.com>
2023-12-15 09:51:04 +0000

    save

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountid=1234
+let accountemail="vara@gmail.com"
+var accountpassword="123453"
+acountcity="jaipur"
+let accountstate;
+
+//accountid=2//not allowed
+accountemail="hc@gmail.com"
+accountpassword="243w6"
+acountcity="bangaluru"
+
+console.log(accountid);
+console.table([accountemail,accountid.accountid,acountcity,accountstate])
+//prefer not to use var
