019dc8304ca9951e65739d5e2bda71a2192d6391
vyshu12-3 <152533992+vyshu12-3@users.noreply.github.com>
2023-12-16 07:53:21 +0000

    commmet

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newre js
+
+//alert(3+3)
+//we are using note.js not brouser
+
+console.log(3+3); console.log("vara")
+//code redability should be followed
+  
+let name="varalakshmi"
+let age=18
+let isloggin=true
+//number=>2to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standard values
+//undefined=>
+console.log(typeof name)
+
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2!=1);
+
+
+//console.log("2">1);
+//console.log("02">1);
+
+console.log(null>0)
+console.log(null>=0)//null is converted to zero
+console.log(null==0)
+
+console.log(undefined==0)
+
+//===  it will check data type
+console.log("2"==2)
