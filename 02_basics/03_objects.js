80fb7c0f091867fa64d75c61a901ea786fe96007
vyshu12-3 <152533992+vyshu12-3@users.noreply.github.com>
2023-12-21 15:19:59 +0000

    change

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+//singleton
+//object litrels
+//Object.create
+const jsuser={
+    name:"hitesh",
+    "full name":"vara chowdary",
+    age:18,
+    loc:"jaipur",
+    email:"hitest.com",
+    islogedin:false,
+    lastlogin:["monday","sat"]
+
+}
+console.log(jsuser.email);
+console.log(jsuser["email"]);
+console.log(jsuser["full name"]);
